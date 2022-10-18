1234e79cf0cbb97297f6cb0ff0677dcf71dc8eb0
kristian93gj <46024348+kristian93gj@users.noreply.github.com>
2022-10-18 17:07:17 +0200

    primer commit

---
+++ src/main/java/com/zeta/gateway/ArbitrajeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zeta.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbitrajeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbitrajeGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeta/gateway/ArbitrajeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeta.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbitrajeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
